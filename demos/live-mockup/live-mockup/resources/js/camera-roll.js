f6e12bda629f438af40c1313a4e7c38586bffcb9
Andreas Binnewies <andreasbinnewies@andreas-binnewiess-imac.local>
2014-01-26 12:58:24 -0800

    + Added the rest of the pictures to the camera roll object. + Added the header for the first step of the setup process.

--- demos/live-mockup/live-mockup/resources/js/camera-roll.js
+++ demos/live-mockup/live-mockup/resources/js/camera-roll.js
@@ -52,6 +52,84 @@ var cameraRoll = [
 	}, {
 		num: 187,
 		moment: 101
+	}, {
+		num: 223,
+		moment: 101
+	// Koh lanta
+	}, {
+		num: 225,
+		moment: 102
+	}, {
+		num: 226,
+		moment: 102
+	}, {
+		num: 229,
+		moment: 102
+	}, {
+		num: 230,
+		moment: 102
+	}, {
+		num: 237,
+		moment: 102
+	}, {
+		num: 239,
+		moment: 102
+	// Patong day 1
+	}, {
+		num: 267,
+		moment: 103
+	}, {
+		num: 268,
+		moment: 103
+	}, {
+		num: 269,
+		moment: 103
+	}, {
+		num: 271,
+		moment: 103
+	}, {
+		num: 272,
+		moment: 103
+	}, {
+		num: 275,
+		moment: 103
+	}, {
+		num: 277,
+		moment: 103
+	}, {
+		num: 281,
+		moment: 103
+	}, {
+		num: 291,
+		moment: 103
+	}, {
+		num: 301,
+		moment: 103
+	}, {
+		num: 314,
+		moment: 103
+	}, {
+		num: 330,
+		moment: 103
+	}, {
+		num: 333,
+		moment: 103
+	}, {
+		num: 334,
+		moment: 103
+	}, {
+		num: 338,
+		moment: 103
+	}, {
+		num: 339,
+		moment: 103
+	// New Years
+	}, {
+		num: 342,
+		moment: 104
+	}, {
+		num: 344,
+		moment: 104
 	}
 ];
 
