881dd554a3a75a9910b5e7d19614236031a19a1c
Andreas Binnewies <andreasbinnewies@andreas-binnewiess-imac.local>
2014-01-26 09:26:44 -0800

    Add a js file that will contain the logic for the iPhone "camera roll".

---
+++ demos/live-mockup/live-mockup/resources/js/camera-roll.js
@@ -0,0 +1,49 @@
+// The list of camera roll photos.
+//  - num: Can be turned into the image file name using the formula:
+//    IMG_0num.jpg
+//  - moment: Used to differentiate which photos belong with which moments.
+var cameraRoll = [
+// Kuala Lumpur
+	{
+		num: 151,
+		moment: 100
+	}, {
+		num: 155,
+		moment: 100
+	}, {
+		num: 161,
+		moment: 100
+	}, {
+		num: 163,
+		moment: 100
+	}, {
+		num: 166,
+		moment: 100
+	}, {
+		num: 168,
+		moment: 100
+	}, {
+		num: 178,
+		moment: 100
+	}, {
+		num: 180,
+		moment: 100
+	}, {
+		num: 155,
+		moment: 100
+	},
+	// Speed boat
+	{
+		num: 183,
+		moment: 101
+	}, {
+		num: 184,
+		moment: 101
+	}, {
+		num: 186,
+		moment: 101
+	}, {
+		num: 187,
+		moment: 101
+	}
+];
