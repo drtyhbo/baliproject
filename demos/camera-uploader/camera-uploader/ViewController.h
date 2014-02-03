d581e1b1e87a950b2386b6329428fd119d756beb
Andreas Binnewies <andreasbinnewies@andreas-binnewiess-imac.local>
2014-02-03 09:37:40 -0800

    Adding a new project which uploads camera roll photos to a server.

---
+++ demos/camera-uploader/camera-uploader/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  camera-uploader
+//
+//  Created by Andreas Binnewies on 2/3/14.
+//  Copyright (c) 2014 Andreas Binnewies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ demos/camera-uploader/camera-uploader/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  camera-uploader
+//
+//  Created by Andreas Binnewies on 2/3/14.
+//  Copyright (c) 2014 Andreas Binnewies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
