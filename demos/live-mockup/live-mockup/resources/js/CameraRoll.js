6b676c85a5e4279837668b007776258f334194d6
Andreas Binnewies <andreasbinnewies@andreas-binnewiess-imac.local>
2014-02-10 12:19:55 -0800

    + Store the responses from the api/asset/get ajax call into local storage so   the subsequent responses can be smaller.

--- demos/live-mockup/live-mockup/resources/js/CameraRoll.js
+++ demos/live-mockup/live-mockup/resources/js/CameraRoll.js
@@ -18,7 +18,7 @@ var Asset = Class.extend({
 });
 
 var CameraRoll = {
-	cameraRoll: [] // array of assets
+	cameraRoll: []
 };
 
 /*
@@ -26,20 +26,39 @@ var CameraRoll = {
  * once they've been loaded.
  */
 CameraRoll.init = function(callback) {
+    CameraRoll.loadFromAssetProps(
+            JSON.parse(localStorage.getItem('assets') || "[]"));
+
     Util.makeRequest('api/asset/get/', {
-        uid: Util.GET['uid']
+        uid: Util.GET['uid'],
+        ts: localStorage.getItem('assets-timestamp') || 0
     }, CameraRoll.load.bind(CameraRoll, callback));
 };
 
 /*
  * Called by the backend once the assets have been loaded.
  */
-CameraRoll.load = function(callback, assets) {
-    for (var i = 0, assetProps; assetProps = assets[i]; i++) {
-        CameraRoll.cameraRoll.push(new Asset(assetProps));
-    }
+CameraRoll.load = function(callback, data) {
+    CameraRoll.loadFromAssetProps(data.assets);
+
+    localStorage.setItem('assets-timestamp', data.ts);
+    // Concatenate the new asset properties with the asset properties stored
+    // in local storage.
+    var storedAssetProps = JSON.parse(localStorage.getItem('assets') || "[]");
+    localStorage.setItem('assets',
+            JSON.stringify(data.assets.concat(storedAssetProps)));
+
     if (callback) {
         callback();
+    }
+};
+
+/*
+ * Loads an array of asset properties.
+ */
+CameraRoll.loadFromAssetProps = function(assetProps) {
+    for (var i = 0, props; props = assetProps[i]; i++) {
+        CameraRoll.cameraRoll.push(new Asset(props));
     }
 };
 
